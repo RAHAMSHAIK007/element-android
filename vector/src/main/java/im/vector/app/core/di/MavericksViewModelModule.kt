950a13e2b79243b3ded82b7978f80060d4474f28
Adam Brown <adampsbrown@gmail.com>
2021-12-08 17:01:26 +0000

    clones the login view model domain to the ftue so that we can start overriding and merge login2 behaviour

--- vector/src/main/java/im/vector/app/core/di/MavericksViewModelModule.kt
+++ vector/src/main/java/im/vector/app/core/di/MavericksViewModelModule.kt
@@ -35,6 +35,7 @@ import im.vector.app.features.crypto.verification.emoji.VerificationEmojiCodeVie
 import im.vector.app.features.devtools.RoomDevToolViewModel
 import im.vector.app.features.discovery.DiscoverySettingsViewModel
 import im.vector.app.features.discovery.change.SetIdentityServerViewModel
+import im.vector.app.features.ftue.FTUEViewModel
 import im.vector.app.features.home.HomeActivityViewModel
 import im.vector.app.features.home.HomeDetailViewModel
 import im.vector.app.features.home.PromoteRestrictedViewModel
@@ -444,6 +445,11 @@ interface MavericksViewModelModule {
     @IntoMap
     @MavericksViewModelKey(AccountCreatedViewModel::class)
     fun accountCreatedViewModelFactory(factory: AccountCreatedViewModel.Factory): MavericksAssistedViewModelFactory<*, *>
+
+    @Binds
+    @IntoMap
+    @MavericksViewModelKey(FTUEViewModel::class)
+    fun ftueViewModelFactory(factory: FTUEViewModel.Factory): MavericksAssistedViewModelFactory<*, *>
 
     @Binds
     @IntoMap
