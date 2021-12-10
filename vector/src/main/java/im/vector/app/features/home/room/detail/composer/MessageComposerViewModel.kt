4bc0d07f3eca6eef0bb2c074c7f8a4ba4deabb73
Valere <valeref@matrix.org>
2021-12-10 15:52:04 +0100

    Support misconfigured room encryption

--- vector/src/main/java/im/vector/app/features/home/room/detail/composer/MessageComposerViewModel.kt
+++ vector/src/main/java/im/vector/app/features/home/room/detail/composer/MessageComposerViewModel.kt
@@ -38,6 +38,7 @@ import im.vector.app.features.session.coroutineScope
 import im.vector.app.features.settings.VectorPreferences
 import im.vector.app.features.voice.VoicePlayerHelper
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
@@ -49,6 +50,7 @@ import org.matrix.android.sdk.api.session.events.model.toContent
 import org.matrix.android.sdk.api.session.events.model.toModel
 import org.matrix.android.sdk.api.session.room.model.PowerLevelsContent
 import org.matrix.android.sdk.api.session.room.model.RoomAvatarContent
+import org.matrix.android.sdk.api.session.room.model.RoomEncryptionAlgorithm
 import org.matrix.android.sdk.api.session.room.model.RoomMemberContent
 import org.matrix.android.sdk.api.session.room.model.message.MessageType
 import org.matrix.android.sdk.api.session.room.powerlevels.PowerLevelsHelper
@@ -57,6 +59,8 @@ import org.matrix.android.sdk.api.session.room.timeline.getLastMessageContent
 import org.matrix.android.sdk.api.session.room.timeline.getRelationContent
 import org.matrix.android.sdk.api.session.room.timeline.getTextEditableContent
 import org.matrix.android.sdk.api.session.space.CreateSpaceParams
+import org.matrix.android.sdk.flow.flow
+import org.matrix.android.sdk.flow.unwrap
 import timber.log.Timber
 
 class MessageComposerViewModel @AssistedInject constructor(
@@ -76,7 +80,7 @@ class MessageComposerViewModel @AssistedInject constructor(
 
     init {
         loadDraftIfAny()
-        observePowerLevel()
+        observePowerLevelAndEncryption()
         subscribeToStateInternal()
     }
 
@@ -139,12 +143,30 @@ class MessageComposerViewModel @AssistedInject constructor(
         }
     }
 
-    private fun observePowerLevel() {
-        PowerLevelsFlowFactory(room).createFlow()
-                .setOnEach {
-                    val canSendMessage = PowerLevelsHelper(it).isUserAllowedToSend(session.myUserId, false, EventType.MESSAGE)
-                    copy(canSendMessage = canSendMessage)
+    private fun observePowerLevelAndEncryption() {
+        combine(
+                PowerLevelsFlowFactory(room).createFlow(),
+                room.flow().liveRoomSummary().unwrap()
+        ) { pl, sum ->
+            val canSendMessage = PowerLevelsHelper(pl).isUserAllowedToSend(session.myUserId, false, EventType.MESSAGE)
+            if (canSendMessage) {
+                val isE2E = sum.isEncrypted
+                if (isE2E) {
+                    val roomEncryptionAlgorithm = sum.roomEncryptionAlgorithm
+                    if (roomEncryptionAlgorithm is RoomEncryptionAlgorithm.UnsupportedAlgorithm) {
+                        CanSendStatus.UnSupportedE2eAlgorithm(roomEncryptionAlgorithm.name)
+                    } else {
+                        CanSendStatus.Allowed
+                    }
+                } else {
+                    CanSendStatus.Allowed
                 }
+            } else {
+                CanSendStatus.NoPermission
+            }
+        }.setOnEach {
+            copy(canSendMessage = it)
+        }
     }
 
     private fun handleEnterQuoteMode(action: MessageComposerAction.EnterQuoteMode) {
--- vector/src/main/java/im/vector/app/features/roommemberprofile/RoomMemberProfileController.kt
+++ vector/src/main/java/im/vector/app/features/roommemberprofile/RoomMemberProfileController.kt
@@ -94,11 +94,14 @@ class RoomMemberProfileController @Inject constructor(
 
     private fun buildSecuritySection(state: RoomMemberProfileViewState) {
         // Security
-        buildProfileSection(stringProvider.getString(R.string.room_profile_section_security))
         val host = this
 
         if (state.isRoomEncrypted) {
-            if (state.userMXCrossSigningInfo != null) {
+            if (!state.isAlgorithmSupported) {
+                // TODO find sensible message to display here
+                // For now we just remove the verify actions as well as the Security status
+            } else if (state.userMXCrossSigningInfo != null) {
+                buildProfileSection(stringProvider.getString(R.string.room_profile_section_security))
                 // Cross signing is enabled for this user
                 if (state.userMXCrossSigningInfo.isTrusted()) {
                     // User is trusted
@@ -151,6 +154,8 @@ class RoomMemberProfileController @Inject constructor(
                     }
                 }
             } else {
+                buildProfileSection(stringProvider.getString(R.string.room_profile_section_security))
+
                 buildProfileAction(
                         id = "learn_more",
                         title = stringProvider.getString(R.string.room_profile_section_security_learn_more),
@@ -161,6 +166,8 @@ class RoomMemberProfileController @Inject constructor(
                 )
             }
         } else {
+            buildProfileSection(stringProvider.getString(R.string.room_profile_section_security))
+
             genericFooterItem {
                 id("verify_footer_not_encrypted")
                 text(host.stringProvider.getString(R.string.room_profile_not_encrypted_subtitle))
